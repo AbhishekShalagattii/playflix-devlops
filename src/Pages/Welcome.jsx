7990b37434cb22dc64e4c242df661154287280d1
AbhishekSH <abhisheksh327507@gmail.com>
2026-04-13 11:14:47 +0530

    new changes

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -32,7 +32,7 @@ function Welcome() {
             <Fade duration={2000}>
               <h1>Welcom To Devops pipeline</h1>
               <h1 className="mb-3 text-3xl font-semibold text-center text-white sm:text-4xl md:text-6xl">
-                Unlimited movies and series , Line manipulated to showcase a triger for jenkins and ci cd
+                Unlimited movies and series , Enjoy your free time 
               </h1>
               <h1 className="mb-4 text-xl text-center text-stone-400 font-light sm:text-2xl">
                 Watch anywahere.Cancel anytime
